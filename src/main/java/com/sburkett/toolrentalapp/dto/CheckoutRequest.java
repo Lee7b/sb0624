8de95b1a5abc594295f363182d7f5cf2deb169d4
Sam Burkett <sburk377@gmail.com>
2024-06-06 09:32:00 -0500

    Add more request validations for null values

--- src/main/java/com/sburkett/toolrentalapp/dto/CheckoutRequest.java
+++ src/main/java/com/sburkett/toolrentalapp/dto/CheckoutRequest.java
@@ -5,14 +5,17 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
 public class CheckoutRequest {
+    @NotBlank
     private String toolCode;
     @Min(value = 1, message = "rentalDayCount must be 1 or greater")
     private int rentalDayCount;
     @Range(min = 0, max = 100, message = "discountPercent must be in range of 0-100")
     private int discountPercent;
+    @NotBlank
     private String checkoutDate;
 }
